5230f7798aa0be61b3abe93bb5df559be3baa3db
Nicolas <contact@nicolas-devynck.fr>
2020-07-07 22:22:03 +0200

    modification des meta balise pour les bot

--- index.php
+++ index.php
@@ -5,18 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bot -->
+    <meta name="Robots" content="all">
+    <meta name="Revisit-After" content="15 days">
+    <meta name="Geography" content="France">
+    <meta name="Distribution" content="global">
     <meta name="Content-Language" content="fr">
-	<meta name="description" content="">
-	<meta name="keywords" content="">
-	<meta name="Copyright" content="Devynck nicolas">
-	<meta name="Author" content="Devynck nicolas">
-	<meta name="Identifier-Url" content="https://www.nicolas-devynck.fr/">
-	<meta name="Revisit-After" content="30 days">
-	<meta name="Robots" content="all">
-	<meta name="Rating" content="general">
-	<meta name="Distribution" content="global">
-	<meta name="Geography" content="France">
-	<meta name="Category" content="">
+    <meta name="Copyright" content="Devynck nicolas">
+    <meta name="Author" content="Devynck nicolas">
+    <meta name="Publisher" content="Devynck nicolas">
+    <meta name="Identifier-Url" content="https://www.nicolas-devynck.fr/">
+    <meta name="Category" content="informatique">
+    <meta name="Subject" content="informatique">
+    <meta name="Rating" content="14 years">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
     <!-- Link -->
     <?php // include "files/pdo.php" ?>
     <script src="files/jquery.js"></script>
@@ -25,7 +27,6 @@
     <link rel="stylesheet" type="text/css" href="files/css.css">
     <link rel="icon" type="image/png" href="files/logo.png" />
     <title>Hello Word</title>
-
 </head>
 <body>
     <h1>Hello Word</h1>
