890c049104d78efd6345a17e6f2381c20063672d
Nicolas <contact@nicolas-devynck.fr>
2021-04-27 11:20:29 +0200

    fix verification de mail

--- php/mail.php
+++ php/mail.php
@@ -21,7 +21,7 @@
 		else { return TRUE; }
 	}
 	function verifMail($fmail) { //Verification du champ mail
-		if (!preg_match('#^[\w.\-]+@[\w.\-]+\.[a-z]{2,6}$#i', $fmail) || strlen($fmail) < 7 || strlen($fmail) > 25 || $fmail == $to) { return FALSE; }
+		if (!preg_match('#^[\w.\-]+@[\w.\-]+\.[a-z]{2,6}$#i', $fmail) || strlen($fmail) < 7 || strlen($fmail) > 25 || $fmail != $to) { return FALSE; }
 		else { return TRUE; }
 	}
 
